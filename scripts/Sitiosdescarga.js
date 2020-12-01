815936cdc7c4df1321d1bb0843f096c7ace42f08
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2020-12-01 11:10:11 -0300

    Add files via upload

---
+++ scripts/Sitiosdescarga.js
@@ -0,0 +1,16 @@
+var Sitiosdescarga = {
+"type": "FeatureCollection",
+"name": "sitios descarga",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nombre": "ISLA NORTE", "descriptio": "Descarga", "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -68.8782018, -32.9285322, 0.0 ] } },
+{ "type": "Feature", "properties": { "Nombre": "EL POZO", "descriptio": "Descarga", "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -68.8798594, -32.9295768, 0.0 ] } },
+{ "type": "Feature", "properties": { "Nombre": "PUESTO JOFRÉ", "descriptio": "Descarga", "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -68.8752816, -32.9430739, 0.0 ] } },
+{ "type": "Feature", "properties": { "Nombre": "CAMINO A MÁCOLA", "descriptio": "Descarga", "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -68.8820463, -32.9469669, 0.0 ] } },
+{ "type": "Feature", "properties": { "Nombre": "POZO IGLESIAS P - PERON", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -68.8804772, -32.9226813, 0.0 ] } },
+{ "type": "Feature", "properties": { "Nombre": "LOS BARRANCOS", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -68.8917557, -32.9200224, 0.0 ] } },
+{ "type": "Feature", "properties": { "Nombre": "TERRENOS DE LOS GUEVARA", "descriptio": "Descarga", "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -68.88039, -32.9275058, 0.0 ] } },
+{ "type": "Feature", "properties": { "Nombre": "POZO URUNDEL", "descriptio": "Descarga", "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -68.8650761, -32.9447322, 0.0 ] } },
+{ "type": "Feature", "properties": { "Nombre": "SIN ID", "descriptio": null, "timestamp": null, "begin": null, "end": null, "altitudeMo": null, "tessellate": -1, "extrude": 0, "visibility": -1, "drawOrder": null, "icon": null }, "geometry": { "type": "Point", "coordinates": [ -68.8840373, -32.9375395, 0.0 ] } }
+]
+}
